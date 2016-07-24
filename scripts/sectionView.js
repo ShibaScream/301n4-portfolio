e32e6b76948512e9c11d704720862bb29f51ad3a
Dan Peters <daniel.r.peters@icloud.com>
2016-07-23 18:02:25 -0700

    I re-factored and started building the site around mobile-first design principles. Figured out the initial styling and wrote much of the base css. The menu nav works, as well.

---
+++ scripts/sectionView.js
@@ -0,0 +1,24 @@
+menuView = {};
+
+menuView.handleMenu = function () {
+  var $menu = $('#menu');
+  var $sections = $('.section-view')
+  
+  $menu.on('click', 'li', function () {
+    $sections.hide();
+    $('#' + $(this).data('section')).show();
+  });
+  
+  $('#menu li:first').click();
+}
+
+menuView.toggleMobileMenu  = function () {
+  $('#mobilemenu').on('click', function () {
+    $('#menu ul').fadeToggle('fast');
+  });
+}
+
+$(document).ready(function() {
+  menuView.handleMenu();
+  menuView.toggleMobileMenu();
+});
