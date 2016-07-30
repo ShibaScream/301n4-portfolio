a14cb6b26e9dee7e90261cfde231ead758d17323
Dan Peters <daniel.r.peters@icloud.com>
2016-07-30 12:18:29 -0700

    Fixed the header image, added additional jquery functionality for hiding and showing menu, added more content

---
+++ scripts/articles.js
@@ -0,0 +1,11 @@
+var articles = [
+  {
+    title: 'Title',
+    language: 'Language',
+    category: 'Category',
+    author: 'Author',
+    authorUrl: '#',
+    publishedOn: '1/1/1900',
+    body: '<p>Body Text Goes Here</p>'
+  },
+];
---
+++ scripts/resumeItems.js
@@ -0,0 +1,11 @@
+var resumeItems = [
+  {
+    title: 'Job Title',
+    company: 'Company',
+    companyURL: '#',
+    companyLogo: 'tempURL',
+    dateFrom: '1/1/2016',
+    dateTo: '7/1/2016',
+    description: '<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>'
+  },
+];
